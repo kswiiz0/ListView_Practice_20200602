160041caff51976f7894690f9f1e94bdc40368b7
kswiiz0 <kswiiz0@gmail.com>
2020-06-03 22:12:11 +0900

    시작 Activity 변경 및 테마(스타일) 변경

---
+++ app/src/main/java/com/phis/listview_practice_20200602/SplashActivity.kt
@@ -0,0 +1,25 @@
+package com.phis.listview_practice_20200602
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : BaseActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        setValues()
+        setupEvents()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+}

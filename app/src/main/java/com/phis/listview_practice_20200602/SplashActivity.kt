8619934ecfa1dedaa0695f35fe96d326fdfaba68
kswiiz0 <kswiiz0@gmail.com>
2020-06-03 22:16:01 +0900

    핸들러 추가 및 기본 사용 소스 추가

--- app/src/main/java/com/phis/listview_practice_20200602/SplashActivity.kt
+++ app/src/main/java/com/phis/listview_practice_20200602/SplashActivity.kt
@@ -1,7 +1,9 @@
 package com.phis.listview_practice_20200602
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SplashActivity : BaseActivity() {
 
@@ -19,6 +21,13 @@ class SplashActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+        val myHandler = Handler()
+        myHandler.postDelayed({
+            val myIntent = Intent(mContext,MainActivity::class.java)
+            startActivity(myIntent)
+            finish()
+        },3000)
 
     }
 
